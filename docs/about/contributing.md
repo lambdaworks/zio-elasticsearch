0e438ec0f8adb93078c24dbcc9c8e6fe5943d618
Velimir Milinković <81649656+mvelimir@users.noreply.github.com>
2022-11-04 09:23:42 +0100

    Set up microsite (#4)
    * Set up the microsite template
    
    * Set up microsite CI
    
    * Change doc titles
    
    * Fix broken links
    
    * Remove empty lines
    
    * Change from TODO to TBD

---
+++ docs/about/contributing.md
@@ -0,0 +1,37 @@
+---
+id: about_contributing
+title: "Contributing"
+---
+
+We welcome contributions from anybody wishing to participate. All code or documentation that is provided must be licensed under [Apache 2.0](https://github.com/lambdaworks/zio-elasticsearch/blob/main/LICENSE).
+
+## General Workflow
+
+1. Make sure you can license your work under Apache 2.0.
+
+2. Before starting work, make sure there is a ticket on the issue, and if not, make sure to create one. It can help accelerate the acceptance process if the change is agreed upon.
+
+3. If you don't have write access to the repository, you should do your work on a local branch of your own fork. If you do have write access to the repository, you should avoid working directly on the main branch.
+   
+4. When your work is completed, verify it with following commands:
+
+```
+sbt check
+sbt +test
+```
+
+5. Submit a Pull Request.
+
+6. Anyone can comment on a Pull Request, and you are expected to answer questions or to incorporate feedback.
+
+## General Guidelines
+
+- We recommend for the work to be accompanied by unit tests.
+
+- The commit messages should be clear and short, and if more details are needed, specify a body.
+
+- Follow the structure of the code in this repository, and the formatting rules used. You can format your code properly with the following command:
+
+```
+sbt prepare
+```
---
+++ website/docusaurus.config.js
@@ -0,0 +1,105 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+const config = {
+  title: 'ZIO Elasticsearch',
+  tagline: 'Elasticsearch client for ZIO',
+  url: 'https://lambdaworks.github.io',
+  baseUrl: '/zio-elasticsearch/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.png',
+  organizationName: 'lambdaworks',
+  projectName: 'zio-elasticsearch',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en']
+  },
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          path: '../modules/zio-elasticsearch-docs/target/mdoc',
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            ({ docPath }) => `https://github.com/lambdaworks/zio-elasticsearch/edit/main/docs/${docPath}`
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      })
+    ]
+  ],
+  themeConfig:
+    ({
+      navbar: {
+        title: 'ZIO Elasticsearch',
+        logo: {
+          alt: 'lambdaworks',
+          src: 'img/logo.svg',
+          srcDark: 'img/logo-dark.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'overview/overview_index',
+            position: 'right',
+            label: 'Overview'
+          },
+          {
+            type: 'doc',
+            docId: 'about/about_index',
+            position: 'right',
+            label: 'About'
+          },
+          {
+            href: 'https://github.com/lambdaworks/zio-elasticsearch',
+            label: 'GitHub',
+            position: 'right'
+          }
+        ]
+      },
+      footer: {
+        style: 'dark',
+        logo: {
+          alt: 'lambdaworks',
+          src: 'img/logo-footer.svg',
+          href: 'https://www.lambdaworks.io/'
+        },
+        links: [
+          {  
+            title: 'GitHub',
+            items: [
+              {
+                html: `
+                <a href="https://github.com/lambdaworks/zio-elasticsearch">
+                  <img src="https://img.shields.io/github/stars/lambdaworks/zio-elasticsearch?style=social" alt="github" />
+                </a>
+              `
+              }
+            ]
+          },
+          {
+            title: 'Additional resources',
+            items: [
+              {
+                label: 'Scaladoc of ZIO Elasticsearch',
+                to: 'pathname:///zio-elasticsearch/api/'
+              }
+            ]
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} LambdaWorks d.o.o.`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+	additionalLanguages: ['java', 'scala']
+      }
+    })
+};
+
+module.exports = config;
