a88709314afe61df5984adcea2c13c5f53914c52
Dragutin Marjanović <dmarjanovic94@gmail.com>
2022-11-30 19:18:30 +0100

    Remove example app

--- modules/example/src/main/scala/example/ExampleDocument.scala
+++
@@ -1,9 +0,0 @@
-package example
-
-import zio.schema.{DeriveSchema, Schema}
-
-final case class ExampleDocument(id: String, name: String, count: Int)
-
-object ExampleDocument {
-  implicit val schema: Schema[ExampleDocument] = DeriveSchema.gen[ExampleDocument]
-}
