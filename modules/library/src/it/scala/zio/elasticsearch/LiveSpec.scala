42469c9102b30b3c049a5b3799bf3b86f476c376
mvelimir <velimirmilinkovic1@gmail.com>
2022-12-02 14:30:41 +0100

    Generate document ID for every test

--- modules/library/src/it/scala/zio/elasticsearch/LiveSpec.scala
+++ modules/library/src/it/scala/zio/elasticsearch/LiveSpec.scala
@@ -4,6 +4,8 @@ import sttp.client3.httpclient.zio.HttpClientZioBackend
 import zio.elasticsearch.ElasticError.DocumentRetrievingError.{DecoderError, DocumentNotFound}
 import zio.schema.{DeriveSchema, Schema}
 import zio.test._
+import zio._
+import java.util.UUID
 
 object LiveSpec extends ZIOSpecDefault {
 
@@ -16,33 +18,37 @@ object LiveSpec extends ZIOSpecDefault {
   implicit val schema2: Schema[TestDocument2] = DeriveSchema.gen[TestDocument2]
 
   private val docIndex = IndexName("indexname")
-  private val docId    = DocumentId("documentId1")
-  private val docId2   = DocumentId("documentId2")
 
   private val document = TestDocument1("123", "Doc123", 1)
+
+  private val uuid: UIO[DocumentId] = ZIO.succeed(DocumentId(UUID.randomUUID().toString))
 
   override def spec: Spec[TestEnvironment, Any] =
     suite("live test")(
       suite("get document by id")(
         test("unsuccessfully get document by id if it does not exists") {
           val doc = for {
-            doc <- ElasticRequest.getById[TestDocument1](docIndex, docId2).routing("10").execute
+            docId <- uuid
+            doc   <- ElasticRequest.getById[TestDocument1](docIndex, docId).routing("10").execute
           } yield doc
 
           assertZIO(doc)(Assertion.equalTo(Left(DocumentNotFound)))
         },
         test("unsuccessfully get document by id if decoder error happens") {
           val doc = for {
-            _   <- ElasticRequest.upsert[TestDocument1](docIndex, docId, document).routing("10").execute
-            doc <- ElasticRequest.getById[TestDocument2](docIndex, docId).routing("10").execute
+            docId <- uuid
+            _     <- ElasticRequest.upsert[TestDocument1](docIndex, docId, document).routing("10").execute
+            doc   <- ElasticRequest.getById[TestDocument2](docIndex, docId).routing("10").execute
           } yield doc
 
           assertZIO(doc)(Assertion.equalTo(Left(DecoderError(".desc(missing)"))))
         },
         test("successfully get document by id") {
           val doc = for {
-            _   <- ElasticRequest.upsert[TestDocument1](docIndex, docId, document).routing("10").execute
-            doc <- ElasticRequest.getById[TestDocument1](docIndex, docId).routing("10").execute
+            docId <- uuid
+            _     <- Console.printLine(docId)
+            _     <- ElasticRequest.upsert[TestDocument1](docIndex, docId, document).routing("10").execute
+            doc   <- ElasticRequest.getById[TestDocument1](docIndex, docId).routing("10").execute
           } yield doc
 
           assertZIO(doc)(Assertion.equalTo(Right(document)))
