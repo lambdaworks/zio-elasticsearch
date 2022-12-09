3d47a02cc50ba052b754d514b75887119b24baeb
Dimitrije Bulaja <dbulaja98@gmail.com>
2022-12-09 15:04:01 +0100

    Fix code remarks

--- modules/library/src/test/scala/zio/elasticsearch/IndexNameSpec.scala
+++ modules/library/src/test/scala/zio/elasticsearch/IndexNameSpec.scala
@@ -1,5 +1,6 @@
 package zio.elasticsearch
 
+import zio.prelude.Newtype.unsafeWrap
 import zio.prelude.Validation
 import zio.test.Assertion.equalTo
 import zio.test._
@@ -9,33 +10,42 @@ object IndexNameSpec extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment, Any] =
     suite("IndexName validation")(
       test("succeed for valid string") {
-        assert(IndexName.make("index-name"))(equalTo(Validation.succeed(IndexName("index-name"))))
+        check(genString(5, 20)) { name =>
+          assert(IndexName.make(name))(equalTo(Validation.succeed(unsafeWrap(IndexName)(name))))
+        }
       },
       test("fail for string containing upper letter") {
-        val name = "Index-name"
-        assert(IndexName.make(name))(equalTo(Validation.fail(indexNameFailureMessage(name))))
+        check(genString(5, 20)) { name =>
+          val invalidName = s"A$name"
+          assert(IndexName.make(invalidName))(equalTo(Validation.fail(indexNameFailureMessage(invalidName))))
+        }
       },
       test("fail for string containing charachter '*'") {
-        val name = "index*name"
-        assert(IndexName.make(name))(equalTo(Validation.fail(indexNameFailureMessage(name))))
+        check(genString(5, 20)) { name =>
+          val invalidName = s"*$name"
+          assert(IndexName.make(invalidName))(equalTo(Validation.fail(indexNameFailureMessage(invalidName))))
+        }
       },
       test("fail for string containing charachter ':'") {
-        val name = "index:name"
-        assert(IndexName.make(name))(equalTo(Validation.fail(indexNameFailureMessage(name))))
+        check(genString(5, 20)) { name =>
+          val invalidName = s":$name"
+          assert(IndexName.make(invalidName))(equalTo(Validation.fail(indexNameFailureMessage(invalidName))))
+        }
       },
       test("fail for string starting with charachter '-'") {
-        val name = "-index.name"
-        assert(IndexName.make(name))(equalTo(Validation.fail(indexNameFailureMessage(name))))
+        check(genString(5, 20)) { name =>
+          val invalidName = s"-$name"
+          assert(IndexName.make(invalidName))(equalTo(Validation.fail(indexNameFailureMessage(invalidName))))
+        }
       },
       test("fail for string '.'") {
         val name = "."
         assert(IndexName.make(name))(equalTo(Validation.fail(indexNameFailureMessage(name))))
       },
       test("fail for string longer than 255 bytes") {
-        check(Gen.stringN(256)(Gen.alphaChar)) { name =>
-          val lowerCaseStr = name.toLowerCase()
-          assert(IndexName.make(lowerCaseStr.toLowerCase))(
-            equalTo(Validation.fail(indexNameFailureMessage(lowerCaseStr.toLowerCase)))
+        check(Gen.stringBounded(256, 300)(Gen.alphaChar).map(_.toLowerCase())) { name =>
+          assert(IndexName.make(name))(
+            equalTo(Validation.fail(indexNameFailureMessage(name)))
           )
         }
       }
@@ -51,4 +61,7 @@ object IndexNameSpec extends ZIOSpecDefault {
        |   - Cannot be longer than 255 bytes (note it is bytes, so multi-byte characters will count towards the 255 limit faster).
        |   - Names starting with . are deprecated, except for hidden indices and internal indices managed by plugins.
        |""".stripMargin
+
+  private def genString(min: Int, max: Int): Gen[Any, String] =
+    Gen.stringBounded(min, max)(Gen.alphaChar).map(_.toLowerCase())
 }
