3ecefd6014a5b9cd83faa10ef59f7d6e369a22e5
Dragutin Marjanović <dmarjanovic94@gmail.com>
2022-12-23 10:26:17 +0100

    Address code remarks

--- modules/example/src/main/scala/example/RepositoriesElasticsearch.scala
+++ modules/example/src/main/scala/example/RepositoriesElasticsearch.scala
@@ -1,38 +1,41 @@
 package example
 
+import zio._
 import zio.elasticsearch.ElasticError.DocumentRetrievingError.DocumentNotFound
 import zio.elasticsearch.{DocumentId, ElasticExecutor, ElasticRequest, Routing}
-import zio.{RIO, Task, URLayer, ZIO, ZLayer}
 
 final case class RepositoriesElasticsearch(executor: ElasticExecutor) {
 
   def findById(organization: String, id: String): Task[Option[GitHubRepo]] =
     for {
-      routing <- Routing.make(organization).toZIO.mapError(e => new IllegalArgumentException(e))
+      routing <- routingOf(organization)
       req      = ElasticRequest.getById[GitHubRepo](Index, DocumentId(id)).routing(routing)
       res     <- executor.execute(req)
     } yield res.toOption
 
   def create(repository: GitHubRepo): Task[Option[DocumentId]] =
     for {
-      routing <- Routing.make(repository.organization).toZIO.mapError(e => new IllegalArgumentException(e))
+      routing <- routingOf(repository.organization)
       req      = ElasticRequest.create(Index, repository).routing(routing)
       res     <- executor.execute(req)
     } yield res
 
   def upsert(id: String, repository: GitHubRepo): Task[Unit] =
     for {
-      routing <- Routing.make(repository.organization).toZIO.mapError(e => new IllegalArgumentException(e))
+      routing <- routingOf(repository.organization)
       req      = ElasticRequest.upsert(Index, DocumentId(id), repository).routing(routing)
       _       <- executor.execute(req)
     } yield ()
 
   def remove(organization: String, id: String): Task[Either[DocumentNotFound.type, Unit]] =
     for {
-      routing <- Routing.make(organization).toZIO.mapError(e => new IllegalArgumentException(e))
+      routing <- routingOf(organization)
       req      = ElasticRequest.deleteById(Index, DocumentId(id)).routing(routing)
       res     <- executor.execute(req)
     } yield res
+
+  private def routingOf(value: String): IO[IllegalArgumentException, Routing.Type] =
+    Routing.make(value).toZIO.mapError(e => new IllegalArgumentException(e))
 
 }
 
