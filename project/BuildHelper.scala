f3e0ba22cce0968b6847e827eb20d85d9edc73db
ioleo <ioleo@protonmail.com>
2023-03-24 19:33:43 +0000

    Cross-compile to Scala 3

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -4,16 +4,17 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
 
-  val Scala212: String = "2.12.17"
-  val Scala213: String = "2.13.10"
+  val Scala212: String   = "2.12.17"
+  val Scala213: String   = "2.13.10"
+  val ScalaDotty: String = "3.2.2"
 
   def stdSettings(prjName: String) =
     List(
       name                     := s"$prjName",
-      crossScalaVersions       := List(Scala212, Scala213),
+      crossScalaVersions       := List(Scala212, Scala213, ScalaDotty),
       ThisBuild / scalaVersion := Scala213,
-      scalacOptions            := stdOptions ++ extraOptions,
-      semanticdbEnabled        := true,
+      scalacOptions            := stdOptions ++ extraOptions(scalaVersion.value),
+      semanticdbEnabled        := scalaVersion.value != ScalaDotty, // enable SemanticDB
       semanticdbOptions += "-P:semanticdb:synthetics:on",
       semanticdbVersion                                          := scalafixSemanticdb.revision,
       ThisBuild / scalafixScalaBinaryVersion                     := CrossVersion.binaryScalaVersion(scalaVersion.value),
@@ -23,10 +24,53 @@ object BuildHelper {
       autoAPIMappings := true
     )
 
-  private val extraOptions =
-    List("-Ywarn-unused")
+  private val stdOptions = Seq(
+    "-deprecation",
+    "-encoding",
+    "UTF-8",
+    "-feature",
+    "-unchecked"
+  )
 
-  private val stdOptions =
-    List("-deprecation", "-encoding", "UTF-8", "-feature", "-unchecked", "-Xfatal-warnings")
+  private val std2xOptions = Seq(
+    "-language:higherKinds",
+    "-language:existentials",
+    "-explaintypes",
+    "-Yrangepos",
+    "-Xlint:_,-missing-interpolator,-type-parameter-shadow",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard"
+  )
 
+  def extraOptions(scalaVersion: String) =
+    CrossVersion.partialVersion(scalaVersion) match {
+      case Some((3, 2)) =>
+        Seq(
+          "-language:implicitConversions",
+          "-Xignore-scala2-macros"
+        )
+      case Some((2, 13)) =>
+        Seq(
+          "-Ywarn-unused:params,-implicits"
+        ) ++ std2xOptions
+      case Some((2, 12)) =>
+        Seq(
+          "-opt-warnings",
+          "-Ywarn-extra-implicit",
+          "-Ywarn-unused:_,imports",
+          "-Ywarn-unused:imports",
+          "-Ypartial-unification",
+          "-Yno-adapted-args",
+          "-Ywarn-inaccessible",
+          "-Ywarn-infer-any",
+          "-Ywarn-nullary-override",
+          "-Ywarn-nullary-unit",
+          "-Ywarn-unused:params,-implicits",
+          "-Xfuture",
+          "-Xsource:2.13",
+          "-Xmax-classfile-name",
+          "242"
+        )
+      case _ => Seq()
+    }
 }
