912b3661da513994550af6904a8aba3b55101764
Dimitrije Bulaja <dbulaja98@gmail.com>
2023-03-29 15:01:57 +0200

    Remove dummy code

--- modules/example/src/main/scala/example/RepositoriesElasticsearch.scala
+++ modules/example/src/main/scala/example/RepositoriesElasticsearch.scala
@@ -18,11 +18,7 @@ package example
 
 import zio._
 import zio.elasticsearch.ElasticQuery.matchAll
-import zio.elasticsearch.ElasticSort.sortBy
 import zio.elasticsearch.query.ElasticQuery
-import zio.elasticsearch.query.sort.SortOrder.Desc
-import zio.elasticsearch.query.sort.SourceType.NumberType
-import zio.elasticsearch.script.Script
 import zio.elasticsearch.{
   CreationOutcome,
   DeletionOutcome,
@@ -36,13 +32,7 @@ import zio.elasticsearch.{
 final case class RepositoriesElasticsearch(elasticsearch: Elasticsearch) {
 
   def findAll(): Task[List[GitHubRepo]] =
-    elasticsearch
-      .execute(
-        ElasticRequest
-          .search(Index, matchAll)
-          .sortBy(sortBy(Script("doc['forks'].value").lang("painless"), NumberType).order(Desc))
-      )
-      .documentAs[GitHubRepo]
+    elasticsearch.execute(ElasticRequest.search(Index, matchAll)).documentAs[GitHubRepo]
 
   def findById(organization: String, id: String): Task[Option[GitHubRepo]] =
     for {
