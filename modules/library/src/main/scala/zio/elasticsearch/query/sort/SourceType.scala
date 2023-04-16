b3089d91d571ab2e495c21583a19a6f27bacbedd
Dragutin Marjanović <dmarjanovic94@gmail.com>
2023-04-16 18:49:48 +0200

    Remove 'final' modifier from case objects

--- modules/library/src/main/scala/zio/elasticsearch/query/sort/SourceType.scala
+++ modules/library/src/main/scala/zio/elasticsearch/query/sort/SourceType.scala
@@ -19,11 +19,11 @@ package zio.elasticsearch.query.sort
 sealed trait SourceType
 
 object SourceType {
-  final case object NumberType extends SourceType {
+  case object NumberType extends SourceType {
     override def toString: String = "number"
   }
 
-  final case object StringType extends SourceType {
+  case object StringType extends SourceType {
     override def toString: String = "string"
   }
 }
