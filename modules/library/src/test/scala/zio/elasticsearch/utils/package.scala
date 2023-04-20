a32cfe7e95012ce970e6b980b038d6e1385d92ce
mvelimir <velimirmilinkovic1@gmail.com>
2023-04-20 18:03:23 +0200

    Add unsafeWrap utility methods

--- modules/library/src/test/scala/zio/elasticsearch/utils/package.scala
+++ modules/library/src/test/scala/zio/elasticsearch/utils/package.scala
@@ -19,7 +19,7 @@ package zio.elasticsearch
 import zio.json.DecoderOps
 import zio.json.ast.Json
 
-package object utils {
+package object utils extends UnsafeWrapUtil {
 
   final implicit class RichString(private val text: String) extends AnyVal {
     def toJson: Json = text.fromJson[Json].toOption.get
