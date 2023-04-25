4baf0910273f7239826f9b581e846373e835c4c0
markaya <markoristic15@gmail.com>
2023-04-25 10:12:05 +0200

    Support elastic primitive for UUID

---
+++ modules/library/src/test/scala/zio/elasticsearch/ElasticPrimitivesSpec.scala
@@ -0,0 +1,41 @@
+package zio.elasticsearch
+
+import zio.elasticsearch.ElasticQuery._
+import zio.elasticsearch.query.Match
+import zio.test.Assertion.equalTo
+import zio.test._
+
+import java.util.UUID
+
+object ElasticPrimitivesSpec extends ZIOSpecDefault {
+
+  private val FieldName = "field_name"
+  override def spec: Spec[TestEnvironment, Any] =
+    suite("Elastic Primitives")(
+      test("successfully create matches query with String") {
+        assert(matches(FieldName, "string"))(equalTo(Match[Any, String](FieldName, "string", boost = None)))
+      },
+      test("successfully create matches query with BigDecimal") {
+        assert(matches(FieldName, BigDecimal(1)))(
+          equalTo(Match[Any, BigDecimal](FieldName, BigDecimal(1), boost = None))
+        )
+      },
+      test("successfully create matches query with Boolean") {
+        assert(matches(FieldName, true))(equalTo(Match[Any, Boolean](FieldName, true, boost = None)))
+      },
+      test("successfully create matches query with Double") {
+        assert(matches(FieldName, 1.00))(equalTo(Match[Any, Double](FieldName, 1.00, boost = None)))
+      },
+      test("successfully create matches query with Int") {
+        assert(matches(FieldName, 1))(equalTo(Match[Any, Int](FieldName, 1, boost = None)))
+      },
+      test("successfully create matches query with Long") {
+        assert(matches(FieldName, 1.toLong))(equalTo(Match[Any, Long](FieldName, 1.toLong, boost = None)))
+      },
+      test("successfully create matches query with UUID") {
+        val uuid = UUID.randomUUID()
+        assert(matches(FieldName, uuid))(equalTo(Match[Any, UUID](FieldName, uuid, boost = None)))
+      }
+    )
+
+}
