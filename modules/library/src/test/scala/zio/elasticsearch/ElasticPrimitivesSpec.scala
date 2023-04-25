56e70d558981bea0ab1614e33378cd75b78d03dc
markaya <markoristic15@gmail.com>
2023-04-25 10:34:18 +0200

    Fix code remarks

--- modules/library/src/test/scala/zio/elasticsearch/ElasticPrimitivesSpec.scala
+++ modules/library/src/test/scala/zio/elasticsearch/ElasticPrimitivesSpec.scala
@@ -9,34 +9,33 @@ import java.util.UUID
 
 object ElasticPrimitivesSpec extends ZIOSpecDefault {
 
-  private val FieldName = "field_name"
+  private val fieldName = "fieldName"
 
   override def spec: Spec[TestEnvironment, Any] =
     suite("Elastic Primitives")(
       test("successfully create matches query with String") {
-        assert(matches(FieldName, "string"))(equalTo(Match[Any, String](FieldName, "string", boost = None)))
+        assert(matches(fieldName, "string"))(equalTo(Match[Any, String](fieldName, "string", boost = None)))
       },
       test("successfully create matches query with BigDecimal") {
-        assert(matches(FieldName, BigDecimal(1)))(
-          equalTo(Match[Any, BigDecimal](FieldName, BigDecimal(1), boost = None))
+        assert(matches(fieldName, BigDecimal(1)))(
+          equalTo(Match[Any, BigDecimal](fieldName, BigDecimal(1), boost = None))
         )
       },
       test("successfully create matches query with Boolean") {
-        assert(matches(FieldName, true))(equalTo(Match[Any, Boolean](FieldName, true, boost = None)))
+        assert(matches(fieldName, true))(equalTo(Match[Any, Boolean](fieldName, true, boost = None)))
       },
       test("successfully create matches query with Double") {
-        assert(matches(FieldName, 1.00))(equalTo(Match[Any, Double](FieldName, 1.00, boost = None)))
+        assert(matches(fieldName, 1.00))(equalTo(Match[Any, Double](fieldName, 1.00, boost = None)))
       },
       test("successfully create matches query with Int") {
-        assert(matches(FieldName, 1))(equalTo(Match[Any, Int](FieldName, 1, boost = None)))
+        assert(matches(fieldName, 1))(equalTo(Match[Any, Int](fieldName, 1, boost = None)))
       },
       test("successfully create matches query with Long") {
-        assert(matches(FieldName, 1.toLong))(equalTo(Match[Any, Long](FieldName, 1.toLong, boost = None)))
+        assert(matches(fieldName, 1L))(equalTo(Match[Any, Long](fieldName, 1L, boost = None)))
       },
       test("successfully create matches query with UUID") {
         val uuid = UUID.randomUUID()
-        assert(matches(FieldName, uuid))(equalTo(Match[Any, UUID](FieldName, uuid, boost = None)))
+        assert(matches(fieldName, uuid))(equalTo(Match[Any, UUID](fieldName, uuid, boost = None)))
       }
     )
-
 }
