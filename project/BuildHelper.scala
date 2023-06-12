7beba575115de4d2bb7c684338b3bfd24824b615
Scala Steward <scala_steward@virtuslab.com>
2023-06-12 16:33:51 +0000

    Revert commit(s) 75ef5ed

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ object BuildHelper {
 
   val Scala212: String = "2.12.17"
   val Scala213: String = "2.13.10"
-  val Scala3: String   = "3.3.0"
+  val Scala3: String   = "3.2.2"
 
   def stdSettings(prjName: String) =
     List(
