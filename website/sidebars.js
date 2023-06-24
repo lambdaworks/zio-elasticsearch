c5710c7623358074827d15f7d8e8703cf9a70ddd
Miloš Tepić <tepcmils@gmail.com>
2023-06-24 12:48:22 +0200

    Remove unnecessary files

--- website/sidebars.js
+++ website/sidebars.js
@@ -35,10 +35,12 @@ module.exports = {
                 label: 'Elastic Aggregation',
                 items: [
                     'overview/elastic_aggregation',
+                    'overview/aggregations/elastic_aggregation_avg',
                     'overview/aggregations/elastic_aggregation_bucket_selector',
                     'overview/aggregations/elastic_aggregation_bucket_sort',
                     'overview/aggregations/elastic_aggregation_cardinality',
                     'overview/aggregations/elastic_aggregation_max',
+                    'overview/aggregations/elastic_aggregation_min',
                     'overview/aggregations/elastic_aggregation_terms',
                 ],
             },
