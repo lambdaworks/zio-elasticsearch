28cc784a15419b44ba9e3547fa2f4efddfe8e480
Dragutin Marjanović <dmarjanovic94@gmail.com>
2023-09-10 19:35:29 +0200

    Bump Scala version to 3.3.1

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ object BuildHelper {
 
   val Scala212: String = "2.12.18"
   val Scala213: String = "2.13.11"
-  val Scala3: String   = "3.3.0"
+  val Scala3: String   = "3.3.1"
 
   def stdSettings(prjName: String) =
     List(
