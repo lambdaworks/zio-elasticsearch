98f7360dc1ed424e5eedf3b008ec0e499c09869a
Dragutin Marjanović <dmarjanovic94@gmail.com>
2024-03-15 10:46:38 +0100

    Update Scala to 3.3.3 (#405)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ object BuildHelper {
 
   val Scala212: String = "2.12.19"
   val Scala213: String = "2.13.13"
-  val Scala3: String   = "3.3.1"
+  val Scala3: String   = "3.3.3"
 
   def stdSettings(prjName: String) =
     List(
