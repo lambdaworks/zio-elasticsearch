3b0164bca89574e9d6624bf79783e9e50d9b3066
Scala Steward <scala_steward@virtuslab.com>
2024-08-27 16:03:47 +0000

    Update scala3-library to 3.5.0

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ object BuildHelper {
 
   val Scala212: String = "2.12.19"
   val Scala213: String = "2.13.14"
-  val Scala3: String   = "3.4.3"
+  val Scala3: String   = "3.5.0"
 
   def stdSettings(prjName: String) =
     List(
