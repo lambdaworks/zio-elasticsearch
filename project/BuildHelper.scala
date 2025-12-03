960c1357ccca8677e3ba3432860b8670920e68d9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-12-03 16:31:07 +0100

    Update scala-library to 2.13.18 (#712)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -7,7 +7,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
 
   val Scala212: String = "2.12.20"
-  val Scala213: String = "2.13.17"
+  val Scala213: String = "2.13.18"
   val Scala3: String   = "3.7.4"
 
   def stdSettings(prjName: String) =
