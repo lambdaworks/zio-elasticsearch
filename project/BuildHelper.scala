4c46c6c8f9558c75e0d239fc9bb9e16b41a0f508
Scala Steward <scala_steward@virtuslab.com>
2026-02-26 19:11:37 +0000

    Update scala3-library to 3.8.2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ object BuildHelper {
 
   val Scala212: String = "2.12.21"
   val Scala213: String = "2.13.18"
-  val Scala3: String   = "3.8.1"
+  val Scala3: String   = "3.8.2"
 
   def stdSettings(prjName: String) =
     List(
