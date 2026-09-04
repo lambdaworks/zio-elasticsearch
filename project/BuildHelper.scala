9e819057df5dd8a26f60e05cf58d820f7094c8e7
Scala Steward <scala_steward@virtuslab.com>
2026-09-04 20:21:39 +0000

    Update scala-library to 3.9.0

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -7,7 +7,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
 
   val Scala212: String = "2.12.21"
-  val Scala213: String = "2.13.18"
+  val Scala213: String = "3.9.0"
   val Scala3: String   = "3.8.2"
 
   def stdSettings(prjName: String) =
